fix numerical stabilty issue

src/tacotron2/model/attention.py
@@ -157,7 +157,7 @@ class MonotonicLocationSensitiveAttention(LocationSensitiveAttention):
                 # log_cumprod_1_minus_p = self.log_safe_cumprod(1 - p_select)
                 log_attention_weights_prev = torch.log(torch.clamp(prev_attention, min=1e-10, max=1))
                 log_attention_weights = log_p_select + log_cumprod_1_minus_p + torch.logcumsumexp(log_attention_weights_prev - log_cumprod_1_minus_p, dim=1)
-                attention_weights = torch.exp(torch.clamp(log_attention_weight, max=1))
+                attention_weights = torch.exp(torch.clamp(log_attention_weights, max=1))
             else:
                 # hard:
                 above_threshold = (alignment > 0).float() # zero because sigmoid!