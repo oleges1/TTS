5b3e3d58fa45da4640c54e69c9355d6262df7c3c
oleges1 <ya.hef@yandex.ru>
2020-12-04 14:33:33 +0300

    fix inference

--- src/wavenet_infer.py
+++ src/wavenet_infer.py
@@ -15,24 +15,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Training model.')
     parser.add_argument('--config', default='wavenet/configs/ljspeech_wavenet.yml',
                         help='path to config file')
+    parser.add_argument('--weights', help='path to model weights file')
     parser.add_argument('--audio', help='path to audio file')
     args = parser.parse_args()
     with open(args.config, 'r') as stream:
         config = edict(yaml.safe_load(stream))
     model = WaveNet(**config.model)
+    model.load_state_dict(torch.load(args.weights))
+    model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
     audio_transform = AudioEncode(quantization_channels=config.model.get('n_classes', 15))
-    mel = MelSpectrogram()
+    mel = MelSpectrogram().to('cuda' if torch.cuda.is_available() else 'cpu')
     gpu = ToGpu('cuda' if torch.cuda.is_available() else 'cpu')
     preprocess = Pad()
     wav, sr = torchaudio.load(args.audio)
     batch = audio_transform({'audio': wav, 'sample_rate': sr})
-    batch = preprocess(mel(gpu({k : [v] for k, v in batch.items()})))
+    batch = mel(gpu({k : [v] for k, v in batch.items()}))
     with torch.no_grad():
         model.eval()
         predictions = model.generate(
-            x=torch.empty((1, self.quantization_channels, 0), device=batch['audio_quantized'].device).float(),
-            h=batch['mel'],
-            samples=batch['audio_quantized'].shape[-1]
+            x=torch.empty((1, config.model.get('n_classes', 15), 0), device=batch['audio_quantized'][0].device).float(),
+            h=batch['mel'][0],
+            samples=batch['audio_quantized'][0].shape[-1]
         )
     predicted_audio = mu_law_decode_torch(predictions[0]).cpu().numpy()
     torchaudio.save('predicted_audio.wav', predicted_audio, sr)
