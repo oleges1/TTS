fce29fedf2656e6b9d63b6802f855019b2e17ea6
students <aotabisheva@almaren.velkerr.ru>
2020-12-14 02:50:59 +0300

    update gradient-based estimation

--- src/wavegrad.py
+++ src/wavegrad.py
@@ -13,17 +13,17 @@ class OptimizerVocoder():
             .log_()
 
     def inference(self, melspectrogram, iters=1000):
-        x = torch.normal(0, 1e-6, size=((melspectrogram.size(1) - 1) * MelSpectrogramConfig.hop_length, )).to(melspectrogram.device).requires_grad_()
+        x = torch.normal(0, 1e-6, size=((melspectrogram.size(-1) - 1) * MelSpectrogramConfig.hop_length, )).to(melspectrogram.device).requires_grad_()
         optimizer = torch.optim.LBFGS([x], tolerance_change=1e-16)
         criterion = torch.nn.MSELoss()
 
         def closure():
             optimizer.zero_grad()
             mel = self.get_mel(x)
-            loss = criterion(mel, melspectrogram)
+            loss = criterion(mel, melspectrogram[0])
             loss.backward()
             return loss
 
         for i in range(iters):
             optimizer.step(closure=closure)
-        return x
+        return x.detach()[:, None]
